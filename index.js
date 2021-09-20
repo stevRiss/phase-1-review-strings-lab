aa3215e282046190023fddf19fdd5c9680d16e2f
Stephen <stevriss22@gmail.com>
2021-09-20 12:09:11 -0400

    finished assingment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Steven';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = 'Welcome, ' + firstInitial + '!';
